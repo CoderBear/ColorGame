3fb7d0b321961b50c46fef31a399bd0088f635b4
Joe Ortega <CoderBear@users.noreply.github.com>
2018-05-24 11:18:39 +0930

    0.0.004
    Now added Easy and Hard modes to the game

--- colorgame.js
+++ colorgame.js
@@ -1,3 +1,4 @@
+var numSquares = 6;
 var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -5,10 +6,40 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.getElementById("reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++) {
+		if(colors[i]) {
+			squares[i].style.background = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+});
+
+hardBtn.addEventListener("click", function(){
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.display = "block";
+	}
+});
 
 resetButton.addEventListener("click", function(){
 	// generate all new colors
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numSquares);
 	// pick a new random color
 	pickedColor = pickColor();
 	//change colorDisplay to match pickedColor
@@ -16,7 +47,11 @@ resetButton.addEventListener("click", function(){
 	// change colors of squares
 	for(var i = 0; i < squares.length; i++) {
 		// add initial colors to squares
-		squares[i].style.backgroundColor = colors[i];
+		if(colors[i]) {
+			squares[i].style.background = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
 	}
 	
 	messageDisplay.textContent = "";
