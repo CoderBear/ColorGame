034f62cc6c95dd391ec2af99931a87a01bebae02
Joe Ortega <CoderBear@users.noreply.github.com>
2018-05-24 10:53:37 +0930

    0.0.003
    The game allows for new colors to be generated before selecting.  After guessing correctly a new game can be started.

--- colorgame.js
+++ colorgame.js
@@ -4,8 +4,28 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.getElementById("reset");
+
+resetButton.addEventListener("click", function(){
+	// generate all new colors
+	colors = generateRandomColors(6);
+	// pick a new random color
+	pickedColor = pickColor();
+	//change colorDisplay to match pickedColor
+	colorDisplay.textContent = pickedColor;
+	// change colors of squares
+	for(var i = 0; i < squares.length; i++) {
+		// add initial colors to squares
+		squares[i].style.backgroundColor = colors[i];
+	}
+	
+	messageDisplay.textContent = "";
+	resetButton.textContent = "NEW COLOR";
+	h1.style.backgroundColor = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
+resetButton.textContent = "NEW COLOR";
 
 for(var i = 0; i < squares.length; i++) {
 	// add initial colors to squares
@@ -20,6 +40,7 @@ for(var i = 0; i < squares.length; i++) {
 			messageDisplay.textContent = "Correct!";
 			changeColors(clickedColor);
 			h1.style.backgroundColor = clickedColor;
+			resetButton.textContent = "Play Again?"
 		} else {
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
